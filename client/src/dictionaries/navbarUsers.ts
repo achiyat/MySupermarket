941fb10fd34b04e111eeec7facfb63393557a4f3
achiyat <achiya308@gmail.com>
2024-12-29 18:46:10 +0200

    remove client

--- client/src/dictionaries/navbarUsers.ts
+++
@@ -1,8 +0,0 @@
-// client/src/dictionaries/navbarUsers.ts
-import { Role } from "../types/types";
-
-export const navbarUsers: Record<Role, string[]> = {
-  Administrator: ["home", "profile", "permissions", "management"],
-  Employee: ["home", "profile", "work"],
-  Customer: ["home", "profile", "store"],
-};
