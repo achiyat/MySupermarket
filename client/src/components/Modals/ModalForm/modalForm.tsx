44ac0b86285ab09f99ecfb2337724aaaea06bb0c
achiyat <achiya308@gmail.com>
2025-01-19 20:41:27 +0200

    Add ModalForm component structure

---
+++ client/src/components/Modals/ModalForm/modalForm.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./ModalForm.css";
+
+interface ModalFormProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export const ModalForm: React.FC<ModalFormProps> = ({
+  isOpen,
+  onClose,
+  children,
+}) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-container">
+        <button className="modal-close" onClick={onClose}>
+          &times;
+        </button>
+        <div className="modal-content">{children}</div>
+      </div>
+    </div>
+  );
+};
