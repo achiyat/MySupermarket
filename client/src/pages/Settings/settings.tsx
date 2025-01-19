898da0d9d3499d69bc3aae4f2d355f2465343fd4
achiyat <achiya308@gmail.com>
2025-01-19 19:53:40 +0200

    Update Settings: Change button after request submission and explain process

--- client/src/pages/Settings/settings.tsx
+++ client/src/pages/Settings/settings.tsx
@@ -1,5 +1,5 @@
 // client/src/pages/Settings/settings.tsx
-import React from "react";
+import React, { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { User } from "../../Interfaces/interfaces";
 import { createRequest } from "../../services/api";
@@ -7,6 +7,7 @@ import "./settings.css";
 
 export const Settings: React.FC = () => {
   const { user } = useOutletContext<{ user: User }>();
+  const [isRequestSent, setIsRequestSent] = useState(false);
 
   const handleCreateRequest = async () => {
     // Create the request object
@@ -25,6 +26,7 @@ export const Settings: React.FC = () => {
     // Call the service function to create the request on the server
     try {
       await createRequest(requestData);
+      setIsRequestSent(true); // Update button and process explanation
     } catch (error) {
       console.error("Error creating request:", error);
     }
@@ -39,9 +41,26 @@ export const Settings: React.FC = () => {
           To become a seller and create your own store, please submit a request
           to change your status.
         </p>
-        <button className="create-request-button" onClick={handleCreateRequest}>
-          Create a request to change status
-        </button>
+
+        {!isRequestSent ? (
+          <button
+            className="create-request-button"
+            onClick={handleCreateRequest}
+          >
+            Create a request to change status
+          </button>
+        ) : (
+          <button className="request-sent-button" disabled>
+            Request sent
+          </button>
+        )}
+
+        {isRequestSent && (
+          <p className="request-explanation">
+            The request has been sent. Please wait for the response. You can
+            track the status of the request in the "Requests" area.
+          </p>
+        )}
       </div>
     </div>
   );
