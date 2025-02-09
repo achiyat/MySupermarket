d1a3dac38f8302819d341cc8e2fe2f1f6ab59e90
achiyat <achiya308@gmail.com>
2025-02-09 19:50:02 +0200

    Added modal for Create and Edit Product

---
+++ client/src/components/Modals/ModalProductForm/modalProductForm.tsx
@@ -0,0 +1,33 @@
+// client/src/components/Modals/ModalProductForm/modalProductForm.tsx
+import React from "react";
+import "./modalProductForm.css";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  buttonText: string;
+}
+
+export const ModalProductForm: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  title,
+  buttonText,
+}) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className=".modalProduct-overlay" onClick={onClose}>
+      <div
+        className=".modalProduct-content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h2>{title}</h2>
+        <button className=".modalProduct-button" onClick={onClose}>
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  );
+};
